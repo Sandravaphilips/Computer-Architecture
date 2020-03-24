e31d2f93ba0c9c72fb55237ab0229f09bb14b9ca
Sandrava Philips <lacasandra1@gmail.com>
2020-03-24 17:30:40 +0100

    made corresponding changes to the load method of our cpu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -38,26 +38,42 @@ class CPU:
     def ram_write(self, value, address):
         self.ram[address] = value
 
-    def load(self):
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        try:
+            with open(program) as program:
+                for line in program:
+                    line_split = line.split('#')
+                    value = line_split[0].strip()
+                    
+                    if value == "":
+                        continue
+                    formatted_value = int(value, 2)
+                    
+                    self.ram[address] = formatted_value
+                    address += 1
+        except FileNotFoundError:
+            print(f"{program} not found")
+            sys.exit(1)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -120,8 +136,6 @@ class CPU:
         """Run the CPU."""
         while self.running:
             instruction = self.ram_read(self.pc)
-            operand_a = self.ram_read(self.pc+1)
-            operand_b = self.ram_read(self.pc+2)
             
             self.branch_table[instruction]()
             
